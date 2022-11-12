0936330e0792dfc81bc9ca569d11d86b8f519c23
Luis Andres <luian.me0714@gmail.com>
2022-11-12 18:17:41 -0500

    implementacion de envio de correos con plantillas de sengrid

--- servidor.py
+++ servidor.py
@@ -4,40 +4,64 @@ import json
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
+print(os.environ.get("SENDGRID_API_KEY"))
+print(os.environ.get("NOMBRE"))
+print(os.environ.get("HASH_VALIDATOR"))
+
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
+    print("hhhhhhhh")
     return "Hello, World From Flask!"
+
 
 @app.route("/email", methods=['POST'])
 def email():
+
     print(os.environ.get("SENDGRID_API_KEY"))
-    print(request.form["message"])
     hash = request.form['hash_validator']
 
-    print(os.environ.get("HASH_VALIDATOR"))
-    print(hash)
-   
-    
-    if(hash == os.environ.get("HASH_VALIDATOR")):
+    if (hash == os.environ.get("HASH_VALIDATOR")):
         print("Entrooooooooooooooooooooooooooooooo")
         try:
             email_sender = os.environ.get("EMAIL_SENDER")
             to = request.form['destination']
             subject = request.form['subject']
             message_content = request.form['message']
-            print(to,subject,message_content)
-
+           
+            mensajito = json.loads(message_content)
+         
+            print(type(mensajito))
+           
+          
+            print("#################")
+            print(mensajito["mensaje"])
+            
             try:
                 sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
                 mensaje = Mail(
                 from_email=email_sender,
-                to_emails=to,
-                subject=subject,
-                html_content=message_content)
-            
-            
+                to_emails=to
+                )
+
+                mensaje.dynamic_template_data = {
+                    "asunto": subject,
+                    "mensaje":  mensajito["mensaje"],
+                    "codigo": mensajito["codigo"]
+                }
+                if subject == "Codigo de Verificación":
+                    mensaje.template_id = os.environ.get(
+                        "IDPLANTILLAENVIOCODIGO")
+                elif subject == "Recuperacion de Contraseña":
+                    mensaje.template_id = os.environ.get(
+                        "IDPLANTILLARECUPERARCONTRASENA")
+                elif subject == " Cambio de Contraseña":
+                    mensaje.template_id = os.environ.get(
+                        "IDPLANTILLACAMBIOCONTRASENA")
+
                 respuesta = sg.send(mensaje)
                 print(respuesta.status_code)
                 print(respuesta.body)
@@ -49,7 +73,7 @@ def email():
             return "Faltan datos"
            
     else:
-        return "Hash error"
+        return "Hash"
 
 if __name__=='__main__':
     app.run()
